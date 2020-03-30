1da4cfbafa4b7803b0ce0a90acce70bf3e037145
Renan Santiago <renansantiago2013@hotmail.com>
2020-03-30 18:06:14 -0300

    hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Pedro otario
-        </p>
-        <a
-          className="App-link"
-          href="https://www.facebook.com/pedropaulo.furtado.trumand"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          olha esse otario
-        </a>
-      </header>
-    </div>
+    <h1>HELLO WORLD</h1>
   );
 }
 
