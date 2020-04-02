ae2efefe4e62b776180c6c511a874847d73b7132
Trumand <furtado.pedropaulo@gmail.com>
2020-04-02 01:40:41 -0300

    Finished createpublication

--- src/components/SideProfile.js
+++ src/components/SideProfile.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import {FaArrowLeft} from 'react-icons/fa'
 
 import ImgLogo from '../assets/Logo.png'
 import './SideProfile.css'
 
 export default function SideProfile () {
   return (
-    <div className="sideprofile-contend">
-      <img src={ImgLogo} alt="Be Home" />
-      <h2>Lucas Cardoso</h2>
-      <p>Seu ID: 66609333</p>
-      <button>Minhas publicações</button>
+    <div className="sideprofile-container">
+      <div className="sideprofile-contend">
+        <img src={ImgLogo} alt="Be Home" />
+        <h2>Lucas Cardoso</h2>
+        <p>Seu ID: 66609333</p>
+        <button>Minhas publicações</button>
+      </div>
+      <a href="/"><FaArrowLeft size={20} /></a>
     </div>
   )
 }
