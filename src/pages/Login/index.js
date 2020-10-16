6be8aa7572a9a6647c97dc95072af96612d58fb5
Lucas Cardoso <lucascardoso0051@gmail.com>
2020-10-16 17:36:52 -0300

    added login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,39 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
 
-import './styles.css';
+import "./styles.css";
 
-import LogoImg from '../../assets/Logo.png'
-import BeHomeImg from '../../assets/BeHome.png'
+import LogoImg from "../../assets/Logo.png";
+import BeHomeImg from "../../assets/BeHome.png";
 
 export default function Login() {
-    return (
-        <div className="logon-container">
-            <section className="form">
-                <img src={LogoImg} alt="BeHome"/>
+  const [id, setId] = useState("");
+  async function handleLogin(e) {
+    e.preventDefault();
 
+    const data = {
+      id,
+    };
 
-                <form>
-                    <h1>Login</h1>
+    try {
+      const response = await api.post("sessions", data);
 
-                    <input placeholder="Entre com seu código ID"/>
-                    
-                    <section className='login-btn'>
-                        <Link to="/feed">
-                            <button type="submit">Entrar</button>
-                        </Link>
-                    </section>
+      alert(`Seu ID de acesso: ${response.data.id}`);
+    } catch (err) {
+      alert("Erro no cadastro, tente novamente.");
+    }
+  }
 
-                </form>
-                <section className="register">
-                    <Link to="/Register">
-                        Não tenho cadastro
-                    </Link>
-                </section>
-                  
-               
-            </section>
-            <img src={BeHomeImg} alt="BeHome"/>
-        </div>
-    );
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={LogoImg} alt="BeHome" />
+
+        <form onSubmit={handleLogin}>
+          <h1>Login</h1>
+
+          <input
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            placeholder="Entre com seu código ID"
+          />
+
+          <section className="login-btn">
+            <Link to="/feed">
+              <button type="submit">Entrar</button>
+            </Link>
+          </section>
+        </form>
+        <section className="register">
+          <Link to="/Register">Não tenho cadastro</Link>
+        </section>
+      </section>
+      <img src={BeHomeImg} alt="BeHome" />
+    </div>
+  );
 }
