5df0188a258a3154e84b5c839da702f65924dca4
Lucas Cardoso <lucascardoso0051@gmail.com>
2020-10-16 17:06:33 -0300

    register changes

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,61 +1,62 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import api from '../../services/api';
-import './styles.css';
+import api from "../../services/api";
+import "./styles.css";
 
-import LogoImg from '../../assets/Logo.png'
+import LogoImg from "../../assets/Logo.png";
 
 export default function Register() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-    async function handleRegister(e) {
-        e.preventDefault();
+  async function handleRegister(e) {
+    e.preventDefault();
 
-        const data = {
-            name,
-            email,
-        };
-        
-       try {
-        const response = await api.post('users', data);
+    const data = {
+      name,
+      email
+    };
 
-        alert(`Seu ID de acesso: ${response.data.id}`);
-       } catch (err) {
-           alert('Erro no cadastro, tente novamente.');
-       }
+    try {
+      const response = await api.post("users", data);
+
+      alert(`Seu ID de acesso: ${response.data.id}`);
+    } catch (err) {
+      alert("Erro no cadastro, tente novamente.");
     }
+  }
 
-    return (
-        <div className="register-container">
-            <section>
-                <img src={LogoImg} alt="BeHome"/>
-                
-                <p className="cadastro">Cadastro</p>
-                <p>Faça seu cadastro na plataforma e compartilhe suas experiências durante a quarentena da COVID-19</p>
-                
-                <Link to="/">
-                    Voltar para login
-                </Link>
-            </section>
+  return (
+    <div className="register-container">
+      <section>
+        <img src={LogoImg} alt="BeHome" />
 
-            <form onSubmit={handleRegister}>
-                <input 
-                placeholder="Nome"
-                value={name}
-                onChange={e => setName(e.target.value)}
-                />
+        <p className="cadastro">Cadastro</p>
+        <p>
+          Faça seu cadastro na plataforma e compartilhe suas experiências
+          durante a quarentena da COVID-19
+        </p>
 
-                <input 
-                type="email" 
-                placeholder="Email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                />
+        <Link to="/">Voltar para login</Link>
+      </section>
 
-                <button type="submit">CADASTRAR</button>
-            </form>
-        </div>
-    );
+      <form onSubmit={handleRegister}>
+        <input
+          placeholder="Nome"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <button type="submit">CADASTRAR</button>
+      </form>
+    </div>
+  );
 }
